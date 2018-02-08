7bdfd91b5e6ddc50cf3e715fae2806bef75eff02
snakejet <in.vnuchkov@jet.msk.su>
2018-02-08 15:50:33 +0300

    dasda

--- index.js
+++ index.js
@@ -40,13 +40,12 @@ class Database {
         this.conn = {open: false};
         this.logger = (typeof(logger) == 'object' && typeof(logger.info) == 'function')
             ? logger : this.innerLogger;
-     }
+    }
 
      /**
       * Connect to database
-      * @author Snake
       */
-     async connect(){
+     async open(){
         this.logger.info('Try connect to database');
         try {
             this.conn = await r.connect({
@@ -57,11 +56,64 @@ class Database {
                 db: this.db
             });
             this.logger.info(`Connected`);
+
+            this.conn.on('error', (error)=>{
+                this.logger.error(`database error: ${error.message}`);
+                this.logger.debug(error);
+            });
+
+            this.conn.on('connect', ()=>{
+                this.logger.info(`connected to database`);
+            });
+
+            this.conn.on('timeout', ()=>{
+                this.logger.error(`database socker error`);
+            });
+
+            this.conn.on('close', ()=>{
+                this.logger.info('database connection close');
+            });
+
         } catch (error) {
             this.logger.error(`Error: ${error.message}`);
             this.logger.debug(error);
         }
-     }
+    }
+
+    /**
+     * disconnect from database
+     */
+    async close(wait = true){
+        this.logger.info(`Try disconnect from database`);
+        try {
+            await this.conn.close([{noreplyWait: wait}]);
+        } catch (error) {
+            this.logger.error(`Error: ${error.message}`);
+            this.logger.debug(error);
+        }
+    }
+
+    /**
+     * force connection close
+     */
+
+    async forceClose(){
+        await this.close(false);
+    }
+
+    /**
+     * change default database
+     * @param {string} db - database name
+     */
+    async use(db){
+        await this.conn.use(db);
+        this.db = db;
+    }
+
+    async tableList(){
+        let TList = await r.db(this.db).tableList().run(this.conn);
+        return TList;
+    }
 }
 
 module.exports.Database = Database;
